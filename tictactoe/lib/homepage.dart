8febaf91f7febd178cc0ef9fe4ebd749e42d11fa
Mattias Lucas <mathiaseluma@gmail.com>
2022-08-27 00:45:27 +0100

    First commit Message

---
+++ tictactoe/lib/homepage.dart
@@ -0,0 +1,249 @@
+import 'package:flutter/material.dart';
+import 'alertdialog.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  int _scoreX = 0;
+  int _scoreO = 0;
+  bool _turnOfO = true;
+  int _filledBoxes = 0;
+  final List<String> _xOrOList = [
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+  ];
+
+  void _tapped(int index) {
+    setState(() {
+      if (_turnOfO && _xOrOList[index] == '') {
+        _xOrOList[index] = 'o';
+        _filledBoxes += 1;
+      } else if (!_turnOfO && _xOrOList[index] == '') {
+        _xOrOList[index] = 'x';
+        _filledBoxes += 1;
+      }
+
+      _turnOfO = !_turnOfO;
+      _checkTheWinner();
+    });
+  }
+
+  void _checkTheWinner() {
+    // check first row
+    if (_xOrOList[0] == _xOrOList[1] &&
+        _xOrOList[0] == _xOrOList[2] &&
+        _xOrOList[0] != '') {
+      _showAlertDialog('Winner', _xOrOList[0]);
+      return;
+    }
+
+    // check second row
+    if (_xOrOList[3] == _xOrOList[4] &&
+        _xOrOList[3] == _xOrOList[5] &&
+        _xOrOList[3] != '') {
+      _showAlertDialog('Winner', _xOrOList[3]);
+      return;
+    }
+
+    // check third row
+    if (_xOrOList[6] == _xOrOList[7] &&
+        _xOrOList[6] == _xOrOList[8] &&
+        _xOrOList[6] != '') {
+      _showAlertDialog('Winner', _xOrOList[6]);
+      return;
+    }
+
+    // check first column
+    if (_xOrOList[0] == _xOrOList[3] &&
+        _xOrOList[0] == _xOrOList[6] &&
+        _xOrOList[0] != '') {
+      _showAlertDialog('Winner', _xOrOList[0]);
+      return;
+    }
+
+    // check second column
+    if (_xOrOList[1] == _xOrOList[4] &&
+        _xOrOList[1] == _xOrOList[7] &&
+        _xOrOList[1] != '') {
+      _showAlertDialog('Winner', _xOrOList[1]);
+      return;
+    }
+
+    // check third column
+    if (_xOrOList[2] == _xOrOList[5] &&
+        _xOrOList[2] == _xOrOList[8] &&
+        _xOrOList[2] != '') {
+      _showAlertDialog('Winner', _xOrOList[2]);
+      return;
+    }
+
+    // check diagonal
+    if (_xOrOList[0] == _xOrOList[4] &&
+        _xOrOList[0] == _xOrOList[8] &&
+        _xOrOList[0] != '') {
+      _showAlertDialog('Winner', _xOrOList[0]);
+      return;
+    }
+
+    // check diagonal
+    if (_xOrOList[2] == _xOrOList[4] &&
+        _xOrOList[2] == _xOrOList[6] &&
+        _xOrOList[2] != '') {
+      _showAlertDialog('Winner', _xOrOList[2]);
+      return;
+    }
+
+    if (_filledBoxes == 9) {
+      _showAlertDialog('Draw', '');
+    }
+  }
+
+  void _showAlertDialog(String title, String winner) {
+    showAlertDialog(
+        context: context,
+        title: title,
+        content: winner == ''
+            ? 'The match ended in a draw'
+            : 'The winner is ${winner.toUpperCase()}',
+        defaultActionText: 'OK',
+        onOkPressed: () {
+          _clearBoard();
+          Navigator.of(context).pop();
+        });
+
+    if (winner == 'o') {
+      _scoreO += 1;
+    } else if (winner == 'x') {
+      _scoreX += 1;
+    }
+  }
+
+  void _clearBoard() {
+    setState(() {
+      for (int i = 0; i < 9; i++) {
+        _xOrOList[i] = '';
+      }
+    });
+
+    _filledBoxes = 0;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.indigoAccent,
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            // creating the ScoreBoard
+            child: Container(
+              color: Colors.indigoAccent,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(30.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        const Text(
+                          'Player X',
+                          style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
+                        ),
+                        Text(
+                          _scoreX.toString(),
+                          style: const TextStyle(
+                              fontSize: 20, color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(30.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        const Text('Player O',
+                            style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white)),
+                        Text(
+                          _scoreO.toString(),
+                          style: const TextStyle(
+                              fontSize: 20, color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 500.0,
+            child: GridView.builder(
+                itemCount: 9,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3),
+                itemBuilder: (BuildContext context, int index) {
+                  return GestureDetector(
+                    onTap: () {
+                      _tapped(index);
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.white),
+                          color: Colors.cyan),
+                      child: Center(
+                        child: Text(
+                          _xOrOList[index],
+                          style: const TextStyle(
+                              color: Colors.white, fontSize: 35),
+                        ),
+                      ),
+                    ),
+                  );
+                }),
+          ),
+          Expanded(
+
+              // Button for Clearing the Enter board
+              // as well as Scoreboard to start allover again
+              child: Container(
+            color: Colors.indigoAccent,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(
+                  width: 350.0,
+                  height: 50.0,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(primary: Colors.blue[800]),
+                    onPressed: _clearBoard,
+                    child: const Text("Clear Score Board"),
+                  ),
+                ),
+              ],
+            ),
+          ))
+        ],
+      ),
+    );
+  }
+}
